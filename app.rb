e4032cf22185ceb404650e6b58022416c8b45947
Sarah Schaller <sarcsch@aol.com>
2017-10-01 15:15:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{@word 1} #{@word 2} #{@word 3} #{@word 4} #{@word 5}"
+    "#{@word1 @word2 @word3 @word4 @word5}"
   end
 
 end
