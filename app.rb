ad30cde2ef659d2efd18158ce26e73a9cb441c47
Sarah Schaller <sarcsch@aol.com>
2017-10-01 15:19:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,19 +27,19 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
-  get '/:operation/:number1/:number2' do 
+  get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i 
+    @number2 = params[:number2].to_i
     if @operation == "add"
       answer = @number1 + @number2
     elsif @operation == "subtract"
-      answer = @number1 - @number2 
+      answer = @number1 - @number2
     elsif @operation == "multiply"
-      answer = @number1 * @number2 
+      answer = @number1 * @number2
     elsif @operation == "divide"
-      answer = @number1 / @number2 
-    end 
+      answer = @number1 / @number2
+    end
     "#{answer}"
   end   
 
