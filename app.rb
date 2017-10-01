14355c4aeb113e0ec1f5d1a65d2cef8ca95e075c
Sarah Schaller <sarcsch@aol.com>
2017-10-01 15:15:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,4 +18,13 @@ class App < Sinatra::Base
     "#{@phrase * @num.to_i}"
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{@word 1} #{@word 2} #{@word 3} #{@word 4} #{@word 5}"
+  end
+
 end
